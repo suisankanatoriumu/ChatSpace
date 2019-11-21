a4b9f6d46b159a753b52e221ff8c7812cf8a15c1
suisankanatoriumu <you@example.com>
2019-11-21 17:21:20 +0900

    非同期通信機能の実装

--- config/application.rb
+++ config/application.rb
@@ -9,14 +9,17 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
+      config.time_zone = 'Tokyo'
+      config.active_record.default_timezone = :local
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.i18n.default_locale = :ja
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.default_locale = :ja
+
   end
 end
